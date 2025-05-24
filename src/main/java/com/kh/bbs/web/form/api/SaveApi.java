fcbbed1340d3629392b8ee7a45d8a886024b9e31
ji-nious <thesolarsystem710@gmail.com>
2025-05-24 18:45:20 +0900

    css, 페이징 넣기

--- src/main/java/com/kh/bbs/web/form/api/SaveApi.java
+++ src/main/java/com/kh/bbs/web/form/api/SaveApi.java
@@ -6,6 +6,9 @@ import lombok.Data;
 
 @Data
 public class SaveApi {
+
+  private Long boardId;
+
   @NotBlank(message = "작성자명은 필수 입력사항입니다.")
   @Size(min=1, max=10, message = "작성자명은 10자를 초과할 수 없습니다.")
   private String writer;
